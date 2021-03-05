b8ea37e6a3481fce9d6d139bfa76a38d89f702a9
hareesh-r <hareeshprogrammer@gmail.com>
2021-03-05 19:55:12 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBVXMf4vA2s3npHJ1JlELHDwH-_LiFxwyc",
+    authDomain: "comebackalums.firebaseapp.com",
+    projectId: "comebackalums",
+    storageBucket: "comebackalums.appspot.com",
+    messagingSenderId: "195792291002",
+    appId: "1:195792291002:web:d46cd6d29c1a0652d2ee83",
+    measurementId: "G-DKEEEH9JWP"
+  };
