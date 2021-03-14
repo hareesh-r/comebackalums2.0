8e2724cc719c0323317d67901f46ad651cd4a895
hareesh-r <hareeshprogrammer@gmail.com>
2021-03-14 23:14:24 +0530

    Show off

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Feed.css";
+import ShowOff from "./ShowOff";
 
 function Feed() {
     return (
         <div className="feed">
+            <ShowOff />
             {/* Show Off */}
             {/* Poster */}
         </div>
---
+++ src/ShowOff.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ShowOff.css";
+
+function ShowOff() {
+    return (
+        <div className="showOff">
+            
+        </div>
+    )
+}
+
+export default ShowOff
