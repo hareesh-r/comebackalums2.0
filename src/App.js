164a057352f4cc407ed075f9d891318e34895f79
hareesh-r <hareeshprogrammer@gmail.com>
2021-03-14 18:14:44 +0530

    work started again

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      <h1>Comeback is coming again</h1>
-
       {/* Header Here*/}
-        <Header />
+      <Header />
           
       
     </div>
   );
 }
-
+// https://console.firebase.google.com/u/1/project/comebackalums/settings/general/web:ZDhlZGI0OTItNWI2YS00MmJmLTkxMjctODQ2ZTY5Y2M4ZDUw
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import "./Header.css";
-import "./img/come-back-logo.png";
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <img src="comebackalums/src/img/come-back-logo.png" alt="Comback logo here" />
+                <img src="url" alt="Comback logo here" />
             </div>
             <div className="header__center"> Amigo</div>
             <div className="header__right">Baba</div>
