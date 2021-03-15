8382f70a376b986d932d0b23249a39f5b93e1b43
hareesh-r <hareeshprogrammer@gmail.com>
2021-03-15 15:42:20 +0530

    post done

--- src/PostHere.js
+++ src/PostHere.js
@@ -1,12 +1,20 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React , { useState } from 'react'
 import "./PostHere.css";
 
 
 function PostHere() {
+    
+    const [input , setInput] = useState("");
+    const [imageUrl , setImageUrl] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // database manupulation pending
+
+        setInput("");
+        setImageUrl("");
     };
     return (
         <div className="postHere">
@@ -14,13 +22,17 @@ function PostHere() {
                 <Avatar />
                 <form>
                     <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                     className="postHere__input" placeholder="Your Testimonial" type="text"/>
-                    <input placeholder="image url (Optional)" />
+                    <input
+                    value={imageUrl}
+                    onChange={(e) => setImageUrl(e.target.value)} placeholder="image url (Optional)" />
                     <button onClick={handleSubmit} type="submit">Hidden Submit</button>
                 </form>
             </div>
             <div className="postHere__boottom">
-                
+
             </div>
         </div>
     )
