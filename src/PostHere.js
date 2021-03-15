b9bfc0c20f2af6ccf976d65979ad104f788db40e
hareesh-r <hareeshprogrammer@gmail.com>
2021-03-15 15:24:42 +0530

    add posting

--- src/PostHere.js
+++ src/PostHere.js
@@ -1,11 +1,27 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import "./PostHere.css";
 
 
 function PostHere() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
     return (
         <div className="postHere">
-            
+            <div className="postHere__top">
+                <Avatar />
+                <form>
+                    <input
+                    className="postHere__input" placeholder="Your Testimonial" type="text"/>
+                    <input placeholder="image url (Optional)" />
+                    <button onClick={handleSubmit} type="submit">Hidden Submit</button>
+                </form>
+            </div>
+            <div className="postHere__boottom">
+                
+            </div>
         </div>
     )
 }
