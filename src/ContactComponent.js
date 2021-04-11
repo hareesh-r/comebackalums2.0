49838d3a46ea60a682a650af105705dc327ea72a
hareesh-r <hareeshprogrammer@gmail.com>
2021-04-11 22:02:12 +0530

    thread visiblity

--- src/ContactComponent.js
+++ src/ContactComponent.js
@@ -2,12 +2,24 @@ import React from 'react'
 import Contact from './Contact'
 import Header from './Header'
 import "./ContactComponent.css";
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import { Link } from 'react-router-dom';
+
 
 function ContactComponent() {
     return (
         <div className="contactComponent">
             <div className="contactComponent__header">
-                <Header /></div>
+                <Header />
+            </div>
+            <div className="show__thread">
+                <h2>Click Here for threads</h2>
+                <Link to="/threads">
+                    <QuestionAnswerIcon />
+                </Link>
+            </div>
+            <br/>
+            <br/>
             <Contact />
         </div>
     )
