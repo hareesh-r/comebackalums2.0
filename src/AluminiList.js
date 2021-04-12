43056728c902aeb9ea00776a2e61c21bc902df88
hareesh-r <hareeshprogrammer@gmail.com>
2021-04-12 10:44:25 +0530

    sort

--- src/AluminiList.js
+++ src/AluminiList.js
@@ -4,18 +4,64 @@ import React, { useEffect, useState } from 'react'
 import db from './firebase';
 import "./AluminiList.css";
 import Contact from './Contact';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 function AluminiList() {
     const [aluminis, setAluminis] = useState([]);
+    const [sortBy, setSortBy] = useState("cgpa");
+
+    const sortByName = () => {
+        setSortBy("aluminiName");
+        console.log("Sort by name");
+        db.collection("aluminis").orderBy("aluminiName", "asc").onSnapshot((snapshot) =>
+            setAluminis(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+
+    }
+    const sortByDesi = () => {
+        setSortBy("designation");
+        console.log("Sort by designation");
+        db.collection("aluminis").orderBy("designation", "asc").onSnapshot((snapshot) =>
+            setAluminis(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+
+    }
+    const sortByReg = () => {
+        setSortBy("regNo");
+        console.log("Sort by regNo");
+        db.collection("aluminis").orderBy("regNo", "asc").onSnapshot((snapshot) =>
+            setAluminis(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+
+    }
+    const sortByCGPA = () => {
+        setSortBy("cgpa");
+        console.log("Sort by cgpa");
+        db.collection("aluminis").orderBy("cgpa", "desc").onSnapshot((snapshot) =>
+            setAluminis(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
+    }
 
     useEffect(() => {
-        db.collection("aluminis").orderBy("cgpa", "desc").onSnapshot((snapshot) =>
+        db.collection("aluminis").orderBy(sortBy, "desc").onSnapshot((snapshot) =>
             setAluminis(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))));
     }, []);
 
     return (
         <div className="aluminilist">
             <Header />
+            <div className="aluminilist__sort">
+                <div class="dropdown">
+                    <button class="dropbtn">Sort By <ArrowDropDownIcon /></button>
+                    <div class="dropdown-content">
+                        <button onClick={sortByDesi} >
+                            Sort by Designation
+                        </button>
+                        <button onClick={sortByName} > Sort by Name
+                        </button>
+                        <button onClick={sortByReg}>Sort by Reg NO
+                        </button>
+                        <button onClick={sortByCGPA}>Sort by CGPA
+                        </button>
+                    </div>
+                </div>
+            </div>
             <div className="aluminilist__body">
                 {aluminis.map((alumini) => (
                     <Alumini className="aluminilist__alumini"
